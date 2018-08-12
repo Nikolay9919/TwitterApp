e54f1ce1f3d871fde8f432854d1d640f80ac6a8a
Nikolay Kuzichev <kuzkuz51@gmail.com>
2018-08-12 18:55:49 +0300

    Add 2 libraries(Picasso and CircleView). Connect Picasso in my main class.

--- TwitterApp/app/src/main/java/com/example/admin/twitterapp/UserInfoActivity.java
+++ TwitterApp/app/src/main/java/com/example/admin/twitterapp/UserInfoActivity.java
@@ -2,12 +2,17 @@ package com.example.admin.twitterapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class UserInfoActivity extends AppCompatActivity {
 
+    private ImageView userImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
-    }
+        userImageView = findViewById(R.id.user_Image_View);
+        Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(userImageView);  }
 }
